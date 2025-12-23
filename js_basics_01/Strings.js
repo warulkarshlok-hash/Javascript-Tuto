d0610bc51ffe0b8d97d2a86d19842cbeadfd95a4
Shlok Warukar <warulkarshlok@gmail.com>
2025-12-23 06:29:32 +0000

    Strings and its methods

---
+++ js_basics_01/Strings.js
@@ -0,0 +1,38 @@
+// -------------------------Strings and its Operations----------------------------
+
+// Methods of Defining Strings: 
+
+const str1 = "Hello"
+const str2 = "World"
+
+// instead : 
+console.log(str1+str2);
+
+// use this : 
+console.log(`The first string is ${str1} and the second string is ${str2}`);
+
+const BigStr="Good Morning"
+
+// Methods on strings 
+
+console.log(BigStr.charAt(3));
+console.log(BigStr.includes("Morning"));
+console.log(BigStr.indexOf("M"));
+console.log(BigStr.replace("Good","Hello"));
+console.log(BigStr.split(" "));
+console.log(BigStr.valueOf());
+console.log(BigStr.length);
+
+// More mehtods : 
+// trim()
+// search()
+// toUpperCase()
+// toLowerCase()
+// and many more
+
+
+
+
+
+
+
